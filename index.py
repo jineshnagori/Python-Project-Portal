0cd3bb7ce0f07b20ed608baad84fcb368013be6e
sudhanshusharma99 <sudhanshusharma20002@gmail.com>
2022-08-17 23:28:05 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,25 @@
+import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/ml")
+def ml():
+    return render_template("ml.html")
+
+@app.route("/dl")
+def dl():
+    return render_template("dl.html")
+
+app.run(debug = True)
